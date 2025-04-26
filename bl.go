643d945d4d620c6b2df8a6ff1d1a6338c4599782
Aaron Ramsey <aaronramsey2000@gmail.com>
2025-04-26 16:40:38 +0100

    feat: command now recursively outputting files with basic formatting

--- bl.go
+++ bl.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var (
+	Reset = "\033[0m"
+	Red   = "\033[31m"
+	Green = "\033[32m"
+)
+
+func buildIterationPrecursor(depth int) string {
+	iterationPrecursor := ""
+	if depth > 0 {
+		iterationPrecursor += "|"
+	}
+	for i := 0; i < depth; i++ {
+		iterationPrecursor += "-"
+	}
+	return iterationPrecursor
+}
+
+func interateFiles(path string, iterationDepth int, maxDepth int) {
+	files, err := ioutil.ReadDir(path)
+	if strings.Contains(path, ".git") || strings.Contains(path, "node_modules") {
+		return
+	}
+	if err != nil {
+		fmt.Println(buildIterationPrecursor, err)
+	}
+
+	for _, file := range files {
+		colour := Reset
+		if file.IsDir() {
+			colour = Green
+		}
+		fmt.Println(buildIterationPrecursor(iterationDepth), file.Mode(), colour, file.Name(), Reset)
+		if file.IsDir() && iterationDepth < maxDepth {
+			interateFiles(path+"/"+file.Name(), iterationDepth+1, maxDepth)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	interateFiles(".", 0, 3)
 }
