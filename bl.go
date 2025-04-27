dde2582ee58b00a3978529e093f70c6b6046e89a
Aaron Ramsey <aaronramsey2000@gmail.com>
2025-04-27 08:19:30 +0100

    feat(depth): add depth flag (d) with default of 3

--- bl.go
+++ bl.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"strings"
 )
 
 var (
-	Reset = "\033[0m"
-	Red   = "\033[31m"
-	Green = "\033[32m"
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+	Gray    = "\033[37m"
+	White   = "\033[97m"
 )
 
 func buildIterationPrecursor(depth int) string {
@@ -24,20 +31,24 @@ func buildIterationPrecursor(depth int) string {
 }
 
 func interateFiles(path string, iterationDepth int, maxDepth int) {
-	files, err := ioutil.ReadDir(path)
+	files, err := os.ReadDir(path)
 	if strings.Contains(path, ".git") || strings.Contains(path, "node_modules") {
 		return
 	}
 	if err != nil {
-		fmt.Println(buildIterationPrecursor, err)
+		fmt.Println(buildIterationPrecursor(iterationDepth), err)
 	}
 
 	for _, file := range files {
 		colour := Reset
 		if file.IsDir() {
-			colour = Green
+			colour = Blue
 		}
-		fmt.Println(buildIterationPrecursor(iterationDepth), file.Mode(), colour, file.Name(), Reset)
+		info, err := file.Info()
+		if err != nil {
+			fmt.Println(buildIterationPrecursor(iterationDepth), err)
+		}
+		fmt.Println(buildIterationPrecursor(iterationDepth), info.Mode(), colour, file.Name(), Reset)
 		if file.IsDir() && iterationDepth < maxDepth {
 			interateFiles(path+"/"+file.Name(), iterationDepth+1, maxDepth)
 		}
@@ -45,5 +56,7 @@ func interateFiles(path string, iterationDepth int, maxDepth int) {
 }
 
 func main() {
-	interateFiles(".", 0, 3)
+	dFlag := flag.Int("d", 3, "Max depth to recursively show files in directories")
+	flag.Parse()
+	interateFiles(".", 0, *dFlag)
 }
